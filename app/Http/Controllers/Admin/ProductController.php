e8866d7d4702e312864eb74bfc6ba16dd60e8c5a
qusai <qusai.nasri@gmail.com>
2022-03-19 21:03:49 +0200

    fix name

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -7,7 +7,7 @@ use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
-class ProductControllert extends Controller
+class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
